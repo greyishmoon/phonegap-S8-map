b1d342521cb8ea1da6692552fcfc93350e8e2a77
greyishmoon <chris@blythe.plus.com>
2017-11-20 15:13:52 +0000

    bug fix

--- www/js/index.js
+++ www/js/index.js
@@ -38,7 +38,7 @@ function onDeviceReady() {
 
 var map;
 var locHome, locCathedral, locUni, locboston;
-var markHome, markCathedral, markUni, markboston, markCurrent;
+var markHome, markCathedral, markUni, markboston;
 
 function runOnLoad(){
     // MAP - Set content window size
@@ -79,17 +79,13 @@ function runOnLoad(){
         title: 'Boston'
     });
     
-    markCurrent = new google.maps.Marker({
-        position: locboston,
-        title: 'You are here!'
-    });
+
     
     // MAP - Place markers
     markHome.setMap(map);
     markCathedral.setMap(map);
     markUni.setMap(map);
     markboston.setMap(map);
-    markCurrent.setMap(map);
     
 
 }
@@ -105,7 +101,6 @@ function successPosition(position) {
     var long = position.coords.longitude;
 	var lat = position.coords.latitude;
     var current = new google.maps.LatLng(lat, long);
-    markCurrent.setPosition(current);
     setloc(current, 17);
 }
 
